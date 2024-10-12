e7cf25cca1c87764e7f0f640f504673544ba7407
blaketorres2000-2 <blaketorres2000@gmail.com>
2024-10-12 10:27:31 -0500

    Initial Commit

---
+++ Game Timer/Game_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_TimerApp.swift
+//  Game Timer
+//
+//  Created by Blake Torres on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Game TimerTests/Game_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Game_TimerTests.swift
+//  Game TimerTests
+//
+//  Created by Blake Torres on 10/12/24.
+//
+
+import Testing
+@testable import Game_Timer
+
+struct Game_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
